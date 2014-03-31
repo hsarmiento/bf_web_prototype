db03edc366e3410a61be2e9e36b96232432aa608
cbaspree <sebastian.bravot@gmail.com>
2014-03-31 17:52:03 -0300

    agrega helpers para manejar los permisos de los usuarios y el archivo config para los roles disponibles en la aplicacion, ademas agrega el password helper que maneja la encriptacion y login

--- application/helpers/login_helper.php
+++ application/helpers/login_helper.php
@@ -28,3 +28,25 @@ if (!function_exists('logged_or_redirect'))
 		return TRUE;
 	}
 }
+
+// if (!function_exists('not_logged_or_redirect'))
+// {
+// 	function not_logged_or_redirect()
+// 	{		
+//        	if(is_logged() === TRUE)
+// 		{
+// 			$CI =& get_instance();
+// 			if ($CI->session->userdata('type') == 'owner')
+// 			{
+// 				$sUrlTo = 'owners/profile';
+// 			}
+// 			elseif ($CI->session->userdata('type') == 'agent')
+// 			{
+// 				$sUrlTo = 'agents/profile';
+// 			}
+// 			redirect(base_url().$sUrlTo);
+// 			return FALSE;
+// 		}
+// 		return TRUE;
+// 	}
+// }
